1e15cc332620099231559e07658a8ca278dd56bb
Ashley Scott <ashley68k@proton.me>
2025-11-06 08:15:12 -0500

    add CLI args

--- zipchar.py
+++ zipchar.py
@@ -1,1 +1,12 @@
 import zipfile
+import os
+import sys
+import argparse
+
+cmd = argparse.ArgumentParser(prog='ZipChar',
+                        description='dump Zip filenames to a C string array')
+cmd.add_argument("-f", "--file", help="Path to the *.zip file to be processed", required=True)
+cmd.add_argument("-s", "--subdir", help="What sub-directory to search", required=True)
+cmd.add_argument("-e", "--ext", help="File extension to filter results by", required=True)
+args = cmd.parse_args()
+
